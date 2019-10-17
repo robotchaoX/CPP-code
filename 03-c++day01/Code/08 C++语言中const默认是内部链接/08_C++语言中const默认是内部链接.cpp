15faa543f84c1cbfbdb1e7e3282daefc0affb5c7
robotchaoX <948037851@qq.com>
2019-10-17 22:39:32 +0800

    ::namespace,using,diff C,reference,pointer,const

---
+++ 03-c++day01/Code/08 C++语言中const默认是内部链接/08_C++语言中const默认是内部链接.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "test2.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    extern const int b;
+    cout << b << endl;
+
+    // system("pause");
+    return EXIT_SUCCESS;
+}
---
+++ 03-c++day01/Code/08 C++语言中const默认是内部链接/test2.hpp
@@ -0,0 +1,2 @@
+// extern const int a = 10; // C++中的const默认内部链接 ,extern提高作用域
+const int b = 10; // C++中的const默认内部链接 ?? 还是外部的吧？？
