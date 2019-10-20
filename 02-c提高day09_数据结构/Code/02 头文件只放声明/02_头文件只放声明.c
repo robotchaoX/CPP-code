5fe072fef5ecbcef0282ff9f52842d44c16ff913
robotchaoX <948037851@qq.com>
2019-10-20 19:51:53 +0800

    stack

---
+++ 02-c提高day09_数据结构/Code/02 头文件只放声明/02_头文件只放声明.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "a.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    // extern int a;
+    printf("a = %d\n", a);
+
+    // system("pause");
+    return EXIT_SUCCESS;
+}
---
+++ 02-c提高day09_数据结构/Code/04 栈的链式存储/LinkStack.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "LinkList.h"
+#include <stdlib.h>
+
+struct StackNode {
+    struct StackNode *next;
+};
+
+struct LStack {
+    struct StackNode header; //头结点
+    int size;
+};
+
+typedef void *LinkStack;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//初始化
+LinkStack Init_LinkStack();
+//入栈
+void Push_LinkStack(LinkStack stack, void *data);
+//出栈
+void Pop_LinkStack(LinkStack stack);
+//获得栈顶元素
+void *Top_LinkStack(LinkStack stack);
+//获得大小
+int Size_LinkStack(LinkStack stack);
+//销毁栈
+void Destroy_LinkStack(LinkStack stack);
+
+#ifdef __cplusplus
+}
+#endif
